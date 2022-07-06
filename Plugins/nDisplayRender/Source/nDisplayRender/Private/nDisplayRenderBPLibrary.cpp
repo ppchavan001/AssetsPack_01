b7fe6b46616a8a48b1a438bee0afe56f6db7b9c9
Pratik Chavan <ppchavan001@gmail.com>
2022-07-06 18:04:19 +0530

    added empty plugin

---
+++ Plugins/nDisplayRender/Source/nDisplayRender/Private/nDisplayRenderBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "nDisplayRenderBPLibrary.h"
+#include "nDisplayRender.h"
+
+UnDisplayRenderBPLibrary::UnDisplayRenderBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UnDisplayRenderBPLibrary::nDisplayRenderSampleFunction(float Param)
+{
+	return -1;
+}
+
