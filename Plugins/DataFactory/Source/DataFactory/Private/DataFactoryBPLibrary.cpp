fe0233aeaff26dd4dcc7746b9c400e6c7e5cc7de
Pratik Chavan <ppchavan001@gmail.com>
2022-07-08 19:08:45 +0530

    added data factory plugin

---
+++ Plugins/DataFactory/Source/DataFactory/Private/DataFactoryBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DataFactoryBPLibrary.h"
+#include "DataFactory.h"
+
+UDataFactoryBPLibrary::UDataFactoryBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UDataFactoryBPLibrary::DataFactorySampleFunction(float Param)
+{
+	return -1;
+}
+
