4c75216f839a2ece8cbb468308783ee989453a0e
ppchavan001 <ppchavan001@gmail.com>
2022-07-18 17:41:07 +0530

    Revert "working on multi delta"
    This reverts commit d6508ed6b3d0d3ffa7e53798bbb334f15d6608b7.

--- Plugins/ExposeEditorFunctions/Source/ExposeRuntimeFunctions/Public/ExposeRuntimeFunctionsBPLibrary.h
+++ Plugins/ExposeEditorFunctions/Source/ExposeRuntimeFunctions/Public/ExposeRuntimeFunctionsBPLibrary.h
@@ -25,39 +25,76 @@
 *	https://wiki.unrealengine.com/Custom_Blueprint_Node_Creation
 */
 
-
-
-UENUM(BlueprintType)
-enum class Axis3D : uint8
-{
-	X UMETA(DisplayName = "X"),
-	Y UMETA(DisplayName = "Y"),
-	Z UMETA(DisplayName = "Z")
-};
-
-USTRUCT(BlueprintType)
-struct FDeltaVertexRequiredInfo
-{
-	GENERATED_BODY()
-
-
-public:
-		UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		Axis3D Axis = Axis3D::Z;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		float MaxDeltaL = 1.0f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		float MaxDeltaR = 1.0f;
-
-};
-
 UCLASS()
 class UExposeRuntimeFunctionsBPLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_UCLASS_BODY()
 
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Get Key From Name", Keywords = "Get Key From Name"), Category = "ExposeRuntimeFunctions | Input")
+	static FKey GetKeyFromName(FName name);
+
+	/*
+		Finds the property by name on the specified object and updates the value. 
+		
+		Supported property types : 
+		BoolProperty		: valid values -> "true" or "false", 
+		NameProperty		: valid values ->  any string will be converted to name, 
+		StrProperty			: valid values ->  any string,
+		TextProperty		: valid values -> Any string will be converted to text,
+		EnumProperty		: valid values -> 0 to (255 or max Enum size which ever is lower), 
+		ArrayProperty		: array of all supported types : valid values -> Comma separated values of the supported type, 
+		NumericProperty		: all numeric types : ex float, int, double, byte
+		StructProperty		: 
+					//	Color					: comma separated keys ex. R:0, G: 25, B:127, A:255 or CSV in same order
+					//	Vector(location, scale)	: comma separated keys ex. LocX: 125, LocY: 778, LocZ : -220 or CSV in same order
+					//	Rotator					: comma separated keys ex. RotX : -75, RotY: 76, RotZ: -650 or CSV in same order
+					//	Transform				: comma separated keys ex. LocX: 400,  RotZ: -100,   ScaleX: 12.2, ScaleY : -54, ScaleZ: 70 
+					//								or CSV in Loc(x,y,z), Rotation(x,y,z), Scale(x,y,z)
+
+
+		Types that are not supported:
+		MapProperty,
+		SetProperty,
+		ObjectPropertyBase,
+		DelegateProperty,
+		InterfaceProperty,
+		MulticastDelegateProperty,
+		NetPropertyHook
+
+
+	*/
+	UFUNCTION(BlueprintCallable, Category = "ExposeRuntimeFunctions | General", meta = (DisplayName = "Set Property Value By Name"))
+	static void SetFPropertyByName(UObject* Object, FName NameOfThePropertyToUpdate, const FString DataToSet);
+
+	static void SetFPropertyValueInternal(FProperty* property, void* Object, const FString DataToSet);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Get Property Class Name"), Category = "ExposeRuntimeFunctions | General")
+	static FString GetFPropertyClassName(UObject* Object, FName PropertyName);
+
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Buffer To String"), Category = "ExposeRuntimeFunctions | Type Conversions")
+	static FString BufferToString(const TArray<uint8>& DataBuffer);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "String To Buffer"), Category = "ExposeRuntimeFunctions | Type Conversions")
+	static void StringToBuffer(const FString& message, TArray<uint8>& DataBuffer);
+
+	/*
+	* String Input:
+		X=-0.000 Y=21.332 Z=-0.476
+		X=-0.000 Y=22.324 Z=-0.476
+		X=-0.000 Y=23.316 Z=-0.476
+		
+
+		Output 
+		Array of vectors with 3 elements
+
+		Note: Will clear VerticesOut before filling it with vectors
+	*/
+	UFUNCTION(BlueprintCallable, Category = "ExposeRuntimeFunctions | VP_Utils")
+	static void ConvertStringToVector(TArray<FString> Lines, TArray<FVector>& VerticesOut);
+
+	UFUNCTION(BlueprintCallable, Category = "ExposeRuntimeFunctions | VP_Utils")
+	static FString ConvertVectorArrayToString(TArray<FString>& LinesOut, TArray<FVector> Vertices);
 	
 	UFUNCTION(BlueprintCallable, Category = "ExposeRuntimeFunctions | VP_Utils")
 	static bool PlotVertices(const TArray<FVector>& VerticesToPlot);
@@ -72,11 +109,7 @@ class UExposeRuntimeFunctionsBPLibrary : public UBlueprintFunctionLibrary
 	// TODO : Find a way to modify in place
 	// Target channel 0 = X, 1 = Y, 2 = Z
 	UFUNCTION(BlueprintCallable, Category = "ExposeRuntimeFunctions | VP_Utils")
-	static TArray<FVector> AddDeltaToMatrixVertices(const TArray<FVector>& VerticesIn, const Axis3D TargetChannel = Axis3D::Z, const float MaxDeltaL = 1, const float MaxDeltaR = 1, const  int32 MatrixWidth = 0, bool InvertDelta = false);
-
-
-	UFUNCTION(BlueprintCallable, Category = "ExposeRuntimeFunctions | VP_Utils")
-		static TArray<FVector> MultiAddDeltaToMatrixVertices(const TArray<FVector>& VerticesIn, const TArray<FDeltaVertexRequiredInfo> DeltaVertexInfo, const  int32 MatrixWidth = 0, bool InvertDelta = false);
+	static TArray<FVector> AddDeltaToMatrixVertices(const TArray<FVector>& VerticesIn, const int32 TargetChannel = 2, const float MaxDeltaL = 1, const float MaxDeltaR = 1, const  int32 MatrixWidth = 0, bool InvertDelta = false);
 
 
 	UFUNCTION(BlueprintCallable, Category = "ExposeRuntimeFunctions | VP_Utils")
