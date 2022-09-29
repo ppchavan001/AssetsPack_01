1ca297dd1538494e10da3da1f3c84f7b0eede9b4
Pratik Chavan <ppchavan001@gmail.com>
2022-09-29 16:40:46 +0530

    added game  c++ module

---
+++ Source/AssetsPack_01/AssetsPack_01.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AssetsPack_01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AssetsPack_01, "AssetsPack_01" );
