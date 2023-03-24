6da54841de45b3ec101a089c2238c69ed7ee2ded
Pratik Chavan <ppchavan001@gmail.com>
2023-03-24 16:14:34 +0530

    added base js script files

---
+++ DataFactory/UnrealJS/test.js
@@ -0,0 +1,18 @@
+// Define any helper functions or variables here
+function myHelperFunction() {
+  // Do something useful...
+}
+
+// Define the main function for your script here
+async function main() {
+  // Your code goes here...
+
+  // Example: Get a reference to the player controller
+  const playerController = unreal.GetPlayerController(0);
+
+  // Example: Log a message to the console
+  console.log("Hello, Unreal.js!");
+}
+
+// Call the main function to start the script
+main();
