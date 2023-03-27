ff8827109d611d9a7958d940e93b27c735af699c
Pratik Chavan <ppchavan001@gmail.com>
2023-03-27 21:23:33 +0530

    working on TPS.u.ts

---
+++ DataFactory/UnrealJSScripts/src/UnrealJS/bootstrap.ts
@@ -0,0 +1,24 @@
+import path from "path"
+
+(function (global)
+{
+    "use strict"
+
+    module.exports = function (filename: string)
+    {
+        //        Context.WriteDTS(Context.Paths[0] + 'typings/ue.d.ts')
+        //        Context.WriteAliases(Context.Paths[0] + 'aliases.js')
+
+
+
+        // Generate typings
+        //Context.WriteDTS(path.join(__dirname, './typings/ue.d.ts'), true);
+        //Context.WriteAliases(path.join(__dirname, '/aliases.js'));
+
+        Context.RunFile('aliases.js')
+        Context.RunFile('polyfill/unrealengine.js')
+        Context.RunFile('polyfill/timers.js')
+
+        require('devrequire')(filename)
+    }
+})(this)
