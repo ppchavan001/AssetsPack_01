4dabaaa4e799de1aa1929e85c17e34a63b7b36c7
Pratik Chavan <ppchavan001@gmail.com>
2023-04-01 17:43:21 +0530

    moved simplified freechar to jspawn

--- DataFactory/UnrealJSScripts/src/Classes/JS_Pawn_Base.u.ts
+++ DataFactory/UnrealJSScripts/src/Classes/JS_Pawn_Base.u.ts
@@ -4,10 +4,8 @@
 @UCLASS()
 class JS_Pawn_Base extends Character
 {
-    @UPROPERTY(EditAnywhere)
     CameraBoom: SpringArmComponent;
 
-    @UPROPERTY(EditAnywhere)
     FollowCamera;
 
 
@@ -20,23 +18,17 @@ class JS_Pawn_Base extends Character
 
 
         this.CapsuleComponent.SetCapsuleSize(42, 96, false);
-        this.CapsuleComponent.SetHiddenInGame(true);
+        this.CapsuleComponent.SetHiddenInGame(true, false);
         this.bUseControllerRotationPitch = false;
         this.bUseControllerRotationRoll = false;
         this.bUseControllerRotationYaw = false;
 
-        this.CharacterMovement.bOrientRotationToMovement = true;
-        this.CharacterMovement.RotationRate = Rotator.MakeRotator(0, 0, 540);
-        this.CharacterMovement.JumpZVelocity = 600;
-        this.CharacterMovement.AirControl = 0.2;
-        this.CharacterMovement.MaxAcceleration = 2048;
-
-        this.CameraBoom = new SpringArmComponent("CameraBoom");
+        this.CameraBoom = new SpringArmComponent();
         this.CameraBoom.AttachParent = this.CapsuleComponent;
         this.CameraBoom.TargetArmLength = 300;
-        this.CameraBoom.bUsePawnControlRotation = true;
+        this.CameraBoom.bUsePawnControlRotation = false;
 
-        this.FollowCamera = new CameraComponent("FollowCamera");
+        this.FollowCamera = new CameraComponent();
         this.FollowCamera.bUsePawnControlRotation = false;
         this.FollowCamera.AttachParent = this.CameraBoom;
 
@@ -79,7 +71,7 @@ class JS_Pawn_Base extends Character
         DataFactoryBPLibrary.AddInputBinding(this,
             AxisName, FunctionName,
             EInputBindingSupportedTypes.AxisBinding,
-            EInputEvent.IE_Axis);
+            EInputEvent.IE_Axis, null);
 
     }
 
@@ -89,8 +81,8 @@ class JS_Pawn_Base extends Character
     }
 
 
-    @KEYBIND(BindAxis, "LookRight_InputAxis")
-    LookRight(value: float)
+    @UFUNCTION()
+    LookRight(value: number)
     {
 
         if (value !== 0)
@@ -99,8 +91,8 @@ class JS_Pawn_Base extends Character
         }
     }
 
-    @KEYBIND(BindAxis, "LookUp_InputAxis")
-    LookUp(value: float)
+    @UFUNCTION()
+    LookUp(value: number)
     {
 
         if (value !== 0)
@@ -109,8 +101,24 @@ class JS_Pawn_Base extends Character
         }
     }
 
-    @KEYBIND(BindAxis, "MoveForward_InputAxis")
-    MoveForward(value: float)
+    @UFUNCTION()
+    MoveForward(value: number)
+    {
+
+        if (value !== 0)
+        {
+            const rotation = this.GetControlRotation();
+            rotation.Pitch = 0;
+            rotation.Roll = 0;
+
+            const forwardVector = rotation.GetForwardVector();
+
+            this.AddMovementInput(forwardVector, value, false);
+        }
+    }
+
+    @UFUNCTION()
+    MoveUp(value: number)
     {
         if (value !== 0)
         {
@@ -124,24 +132,9 @@ class JS_Pawn_Base extends Character
         }
     }
 
-    @KEYBIND(BindAxis, "MoveUp_InputAxis")
-    MoveUp(value: float)
-    {
-        if (value !== 0)
-        {
-            const rotation = this.GetControlRotation();
-            rotation.Pitch = 0;
-            rotation.Roll = 0;
 
-            const forwardVector = rotation.GetForwardVector();
-
-            this.AddMovementInput(forwardVector, value, false);
-        }
-    }
-
-
-    @KEYBIND(BindAxis, "MoveRight_InputAxis")
-    MoveRight(value: float)
+    @UFUNCTION()
+    MoveRight(value: number)
     {
         if (value !== 0)
         {
@@ -155,21 +148,6 @@ class JS_Pawn_Base extends Character
         }
     }
 
-    @UFUNCTION(Server, Unreliable)
-    Server_Attack()
-    {
-        this.Attack();
-    }
-
-    @KEYBIND(BindAction, "Attack", IE_RELEASED)
-    Attack()
-    {
-        if (!this.HasAuthority())
-        {
-            this.Server_Attack();
-            return;
-        }
-    }
 }
 
 export default JS_Pawn_Base;
