dc9ef36d1b800f0cc870c171bd00dd5c77be1f89
Pratik Chavan <ppchavan001@gmail.com>
2023-04-01 17:05:44 +0530

    generating typings and aliases in src

--- DataFactory/UnrealJSScripts/src/UnrealJS/bootstrap.ts
+++ DataFactory/UnrealJSScripts/src/UnrealJS/bootstrap.ts
@@ -12,8 +12,14 @@ import path from "path"
 
 
         // Generate typings
-        //Context.WriteDTS(path.join(__dirname, './typings/ue.d.ts'), true);
-        //Context.WriteAliases(path.join(__dirname, '/aliases.js'));
+
+        // in dist dir
+        // Context.WriteDTS(path.join(__dirname, './typings/ue.d.ts'), true);
+        // Context.WriteAliases(path.join(__dirname, '/aliases.js'));
+
+        // in src
+        // Context.WriteDTS(path.join(__dirname, '../../src/UnrealJS/typings/ue.d.ts'), true);
+        // Context.WriteAliases(path.join(__dirname, '../../src/UnrealJS/aliases.js'));
 
         Context.RunFile('aliases.js')
         Context.RunFile('polyfill/unrealengine.js')
