0c6c4e22ada8e86e0e9bb046151b6435e8420bb8
Pratik Chavan <ppchavan001@gmail.com>
2023-04-03 21:19:53 +0530

    binding FreeChar input from JS

--- DataFactory/UnrealJSScripts/src/TestScripts/FreeCharacterBindings.u.ts
+++ DataFactory/UnrealJSScripts/src/TestScripts/FreeCharacterBindings.u.ts
@@ -1,13 +1,13 @@
 import JS_Pawn_Base from "../Classes/JS_Pawn_Base.u";
+import { SetAxisBinding, SetAxisBindings, SetKeyBinding } from '../lib/Input';
 
 function main(): Function
 {
 
     // Do stuff
-    //DataFactoryBPLibrary.DF_PrintString(undefined, "Hola from : " + __filename, EDataFactoryLogVerbosity.Warning, true, true, 5.0, 3);
+    //DataFactoryBPLibrary.DF_PrintString(null, "Hola from : " + __filename, EDataFactoryLogVerbosity.Warning, true, true, 5.0, 3);
 
-    let fc = GWorld.GetPlayerPawn(0);
-
+    fc_bindInputs();
 
     // clean up the mess
     return function ()
@@ -16,7 +16,34 @@ function main(): Function
 
         //char.DestroyActor();
     };
+
+
 }
+
+
+// binds free character input if its possesed by player controller 0
+function fc_bindInputs(): void
+{
+    let fc = GWorld.GetPlayerPawn(0);
+
+
+    SetKeyBinding(fc, 'L', "LoadFile");
+
+    // Axis name, Function name Bindings
+    let AxisBindings = new Map<string, string>();
+
+    AxisBindings.set("MoveForward_InputAxis", "SetMoveForwardAxisValue");
+    AxisBindings.set("MoveRight_InputAxis", "SetMoveRightAxisValue");
+    AxisBindings.set("MoveUp_InputAxis", "SetMoveUpAxisValue");
+    AxisBindings.set("LookRight_InputAxis", "SetLookRightAxisValue");
+    AxisBindings.set("LookUp_InputAxis", "SetLookUpAxisValue");
+
+
+    SetAxisBindings(fc, AxisBindings);
+
+}
+
+
 
 
 
@@ -37,5 +64,5 @@ try
 } catch (e)
 {
 
-    require("./UnrealJS/bootstrap")('TP_Character_JS.u');
+    require("./UnrealJS/bootstrap")('FreeCharacterBindings.u');
 }
--- DataFactory/UnrealJSScripts/src/lib/Input.ts
+++ DataFactory/UnrealJSScripts/src/lib/Input.ts
@@ -9,6 +9,26 @@ export function SetAxisBinding(obj: UObject, AxisName: string, FunctionName: str
 
 }
 
+export function SetKeyBinding(obj: UObject, KeyName: string, FunctionName: string, InputEvent: EInputEvent = EInputEvent.IE_Released)
+{
+    DataFactoryBPLibrary.AddInputBinding(obj,
+        KeyName, FunctionName,
+        EInputBindingSupportedTypes.KeyBinding,
+        InputEvent, null);
+
+}
+
+
+//export function SetActionBinding(obj: UObject, ActionName: string, FunctionName: string, InputEvent: EInputEvent = EInputEvent.IE_Released)
+export function SetActionBinding(obj: UObject, ActionName: string, FunctionName: string, InputEvent: EInputEvent = EInputEvent.IE_Released)
+{
+    DataFactoryBPLibrary.AddInputBinding(obj,
+        ActionName, FunctionName,
+        EInputBindingSupportedTypes.ActionBinding,
+        InputEvent, null);
+
+}
+
 export function SetAxisBindings(obj: UObject, AxisBindings: Map<string, string>)
 {
 
