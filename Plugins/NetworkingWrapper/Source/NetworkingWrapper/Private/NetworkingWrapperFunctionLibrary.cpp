b1a2ebd38ea3c9e6fd95b9747e221a12d2a0bebe
Pratik Chavan <ppchavan001@gmail.com>
2023-09-18 16:53:03 +0530

    copied dflog to netwrapper

--- Plugins/NetworkingWrapper/Source/NetworkingWrapper/Private/NetworkingWrapperFunctionLibrary.cpp
+++ Plugins/NetworkingWrapper/Source/NetworkingWrapper/Private/NetworkingWrapperFunctionLibrary.cpp
@@ -7,6 +7,11 @@
 #include "Utils/JsonSerializer/ODJsonSerializer.h"
 #include "NetworkingInterface.h"
 #include "Kismet/GameplayStatics.h"
+#include "Logging/LogMacros.h"
+
+#define  CurrentFileName FString("NetworkingWrapperFunctionLibrary.cpp")
+DEFINE_LOG_CATEGORY_STATIC(NETWRAPLOG, Log, All);
+
 
 UObject* UNetworkingWrapperFunctionLibrary::String2UObject(const FString& string, UClass* classType)
 {
@@ -53,5 +58,152 @@ void UNetworkingWrapperFunctionLibrary::UDP_SendObj(const UObject* WorldContextO
 	for (AActor* Actor : ActorsWithInterface)
 	{
 		IUDP_DataSenderInterface::Execute_UDP_SendObject(Actor, Data);
+	}
+}
+
+
+void UNetworkingWrapperFunctionLibrary::NW_PrintString(
+	const UObject* WorldContextObject,
+	const FString InString /*= ""*/,
+	ENetWrapLogVerbosity LogVerbosity /*= EDataFactoryLogVerbosity::Log*/,
+	bool bPrintToScreen /*= true*/,
+	bool bPrintToLog /*= true*/,
+	float Duration /*= 2.f*/,
+	int MaxStackDataDepth  /* = 1*/)
+{
+	if (LogVerbosity == ENetWrapLogVerbosity::NoLogging)
+	{
+		return;
+	}
+
+#pragma region Prefix
+	UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::ReturnNull);
+	FString Prefix;
+	if (World)
+	{
+		if (World->WorldType == EWorldType::PIE)
+		{
+			switch (World->GetNetMode())
+			{
+			case NM_Client:
+				// GPlayInEditorID 0 is always the server, so 1 will be first client.
+				// You want to keep this logic in sync with GeneratePIEViewportWindowTitle and UpdatePlayInEditorWorldDebugString
+				Prefix = FString::Printf(TEXT("Client %d"), GPlayInEditorID);
+				break;
+			case NM_DedicatedServer:
+			case NM_ListenServer:
+				Prefix = FString::Printf(TEXT("Server"));
+				break;
+			case NM_Standalone:
+				break;
+			}
+		}
+	}
+
+
+#pragma  endregion
+
+	FString FinalLogString = Prefix + InString;
+
+#pragma region AddCallerInfo
+
+	static const FBoolConfigValueHelper DisplayPrintStringSource(TEXT("Kismet"), TEXT("bLogPrintStringSource"), GEngineIni);
+	if (DisplayPrintStringSource)
+	{
+
+#if DO_BLUEPRINT_GUARD
+		FString StackData;
+		FBlueprintContextTracker& BlueprintExceptionTracker = FBlueprintContextTracker::Get();
+		if (BlueprintExceptionTracker.GetScriptStack().Num() > 0)
+		{
+			int stackDepth = 0;
+
+			for (int i = BlueprintExceptionTracker.GetScriptStack().Num() - 1; i > -1 && stackDepth < MaxStackDataDepth; --i)
+			{
+				StackData += BlueprintExceptionTracker.GetScriptStack()[i]->GetStackDescription();
+				stackDepth += 1;
+
+				if (stackDepth != MaxStackDataDepth)
+				{
+					StackData += " <- ";
+				}
+
+			}
+
+			FinalLogString = "[" + StackData + "] " + FinalLogString;
+		}
+		else
+		{
+			//ScriptStack += TEXT("\t[Empty] (FFrame::GetScriptCallstack() called from native code)");
+			//const FString SourceObjectPrefix = FString::Printf(TEXT("[%s]"), *GetNameSafe(WorldContextObject));
+			FinalLogString = WorldContextObject->GetName() + FinalLogString;
+
+		}
+
+		// add log verbosity to final log? is added by ue on some verbosities
+		//FinalLogString = "[" + FString(ToString((ELogVerbosity::Type)(LogVerbosity))) + "] " + FinalLogString;
+
+#else
+		//FString TopOfStack = TEXT("Unable to display Script Callstack. Compile with DO_BLUEPRINT_GUARD=1");
+		FinalLogString = WorldContextObject->GetName() + FinalLogString;
+
+#endif
+
+	}
+
+#pragma endregion
+
+	// Log
+#if !NO_LOGGING
+
+	if (bPrintToLog)
+	{
+		GLog->Log(NETWRAPLOG.GetCategoryName(), (ELogVerbosity::Type)(LogVerbosity), &FinalLogString[0]);
+	}
+#endif
+	// Also output to the screen, if possible
+	if (bPrintToScreen)
+	{
+		if (GAreScreenMessagesEnabled)
+		{
+
+			FColor OnScreenTextColor = FColor::White;
+
+			switch (LogVerbosity)
+			{
+			case ENetWrapLogVerbosity::NoLogging:
+				OnScreenTextColor = FColor(0, 0, 0, 0);
+				break;
+			case ENetWrapLogVerbosity::Fatal:
+				OnScreenTextColor = FColor(255, 0, 0, 255);
+				break;
+			case ENetWrapLogVerbosity::Error:
+				OnScreenTextColor = FColor(255, 25, 25, 255);
+				break;
+			case ENetWrapLogVerbosity::Warning:
+				OnScreenTextColor = FColor(255, 255, 0, 255);
+				break;
+			case ENetWrapLogVerbosity::Display:
+				OnScreenTextColor = FColor(255, 255, 255, 255);
+				break;
+			case ENetWrapLogVerbosity::Log:
+				OnScreenTextColor = FColor(230, 230, 230, 255);
+				break;
+			case ENetWrapLogVerbosity::Verbose:
+				OnScreenTextColor = FColor(200, 200, 200, 255);
+				break;
+			case ENetWrapLogVerbosity::VeryVerbose:
+				OnScreenTextColor = FColor(150, 150, 150, 255);
+				break;
+			}
+
+
+			GEngine->AddOnScreenDebugMessage((uint64)-1, Duration, OnScreenTextColor, FinalLogString);
+		}
+		else
+		{
+			UE_LOG(NETWRAPLOG, VeryVerbose, TEXT("%s : %s : %s : Screen messages disabled (!GAreScreenMessagesEnabled).  Cannot print to screen."),
+				*CurrentFileName, *FString(__func__), __LINE__);
+		}
 	}
 }
--- Plugins/NetworkingWrapper/Source/NetworkingWrapper/Public/NetworkingWrapperFunctionLibrary.h
+++ Plugins/NetworkingWrapper/Source/NetworkingWrapper/Public/NetworkingWrapperFunctionLibrary.h
@@ -4,7 +4,52 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "UObject/UObjectGlobals.h"
 #include "NetworkingWrapperFunctionLibrary.generated.h"
+
+
+
+UENUM(BlueprintType)
+enum class ENetWrapLogVerbosity : uint8
+{
+	/** returns without logging */
+	NoLogging = 0,
+
+	/** Always prints a fatal error to console (and log file) and crashes (even if logging is disabled) */
+	Fatal,
+
+	/**
+		* Prints an error to console (and log file).
+		* Commandlets and the editor collect and report errors. Error messages result in commandlet failure.
+		*/
+	Error,
+
+	/**
+	* Prints a warning to console (and log file).
+	* Commandlets and the editor collect and report warnings. Warnings can be treated as an error.
+	*/
+	Warning,
+
+	/** Prints a message to console (and log file) */
+	Display,
+
+	/** Prints a message to a log file (does not print to console) */
+	Log,
+
+	/**
+	* Prints a verbose message to a log file (if Verbose logging is enabled for the given category,
+	* usually used for detailed logging)
+	*/
+	Verbose,
+
+	/**
+	* Prints a verbose message to a log file (if VeryVerbose logging is enabled,
+	* usually used for detailed logging that would otherwise spam output)
+	*/
+	VeryVerbose
+
+};
+#define GETENUMSTRING(etype, evalue) ( (FindObject(ANY_PACKAGE, TEXT(etype), true) != nullptr) ? FindObject(ANY_PACKAGE, TEXT(etype), true)->GetEnumName((int32)evalue) : FString("Invalid - are you sure enum uses UENUM() macro?") )
 
 /**
  *
@@ -21,25 +66,36 @@ public:
 	* the string must be json representation of the object
 	*/
 	UFUNCTION(BlueprintCallable)
-		static UObject* String2UObject(const FString& string, UClass* classType);
+	static UObject* String2UObject(const FString& string, UClass* classType);
 
 	/*
 	* Converts the given object to a json string
 	*/
 	UFUNCTION(BlueprintCallable)
-		static FString UObject2String(const UObject* packet);
+	static FString UObject2String(const UObject* packet);
 
 	/*
 	* calls UDP_Send on all objects with UDP Send interface
 	* with given parameters
 	*/
 	UFUNCTION(BlueprintCallable, Category = "Networking Wrapper | UDP")
-		static void UDP_SendStr(const UObject* WorldContextObject, const FString& Data);
+	static void UDP_SendStr(const UObject* WorldContextObject, const FString& Data);
 
 	/*
-	* calls UDP_Send on all objects with UDP Send interface 
+	* calls UDP_Send on all objects with UDP Send interface
 	* with given parameters
 	*/
 	UFUNCTION(BlueprintCallable, Category = "Networking Wrapper | UDP")
-		static void UDP_SendObj(const UObject* WorldContextObject, const UObject* Data);
+	static void UDP_SendObj(const UObject* WorldContextObject, const UObject* Data);
+
+
+	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContextObject", CallableWithoutWorldContext, Keywords = "log print", AdvancedDisplay = "2", DevelopmentOnly), Category = "Networking Wrapper")
+	static void NW_PrintString(const UObject* WorldContextObject,
+		const FString InString = "",
+		ENetWrapLogVerbosity LogVerbosity = ENetWrapLogVerbosity::Log,
+		bool bPrintToScreen = true,
+		bool bPrintToLog = true,
+		float Duration = 2.f,
+		int MaxStackDataDepth = 1);
+
 };
